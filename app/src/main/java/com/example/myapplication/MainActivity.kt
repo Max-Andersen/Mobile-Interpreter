64e223ac1d64ddd2bec03e57345a3644c305dcfc
devloliconic <kirillevsmirnov@gmail.com>
2022-04-29 15:40:38 +0700

    send file

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.example.myapplication
 
+import android.graphics.Color
+import android.graphics.Color.RED
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,13 +21,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
-        val fragment: ArrayList<Fragment> = arrayListOf(
-            BlankFragment(),
-            variables()
-        )
-        val adapter = ViewPagerAdapter(fragment,this)
-        viewPager.adapter = adapter
+
+        val buttonplus: ImageButton = findViewById(R.id.plus1)
+        val screen: ConstraintLayout = findViewById(R.id.blockscreen)
+        val close: ImageButton = findViewById(R.id.closeBlockScreen)
+        buttonplus.setOnClickListener() {
+            screen.visibility = View.VISIBLE
+            buttonplus.visibility = View.INVISIBLE
+        }
+        close.setOnClickListener(){
+            screen.visibility = View.INVISIBLE
+            buttonplus.visibility = View.VISIBLE
+        }
     }
 
 }
